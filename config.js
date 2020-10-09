9eb0745fda3688e2d175e001c09e6862e3918349
kundouzhishou <kundouzhishou@gmail.com>
2020-10-09 21:36:32 +0800

    config ignore fix

---
+++ config.js
@@ -0,0 +1,21 @@
+config = exports;
+
+config.nginx = {
+    name: '',
+    password: ''
+}
+
+config.miaotixing = {
+    id_uniswap: "",
+    id_bancor_bonding_curve: ""
+}
+
+config.eth_account = {
+    address: '',
+    private_key: ''
+}
+
+config.provider_url_local  = ``;
+config.provider_url_server  = ``;
+config.provider_url_truffle_rinkeby = ""
+config.provider_url_truffle_mainnet = ""
